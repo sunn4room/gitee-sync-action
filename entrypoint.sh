9d8bda45ea08f6899c757faf1bc7b42a1c1ca316
sunn4room <sunn4room@163.com>
2024-03-26 20:15:19 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -l
 
 parse_repo() {
 	if expr "$1" : '[^/]*/[^/]*$' >/dev/null; then
@@ -7,7 +7,7 @@ parse_repo() {
 		COUNT=0
 		echo "$1"
 		while true; do
-			COUNT=$(("$COUNT" + 1))
+			COUNT=$(($COUNT + 1))
 			OUTPUT="$(curl -s "https://gitee.com/api/v5/orgs/$1/repos?type=all&per_page=100&page=$COUNT" | jq -r .[].full_name)"
 			printf '%s' "$OUTPUT"
 			if test "$(printf '%s' "$OUTPUT" | wc -l)" -lt "100"; then
