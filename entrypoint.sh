8ea35ae2b85e27d4e19612bbef52d69ef0bb7904
sunn4room <sunn4room@163.com>
2024-03-26 21:55:26 +0800

    handle error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
+# shellcheck disable=SC2015
+# shellcheck disable=SC2164
 
 parse_repo() {
 	if expr "$1" : '[^/]*/[^/]*$' >/dev/null; then
@@ -20,31 +22,29 @@ mirror_repo() {
 	printf '%s\n' "[$1]"
 	GITEE_REPO="https://${INPUT_USERNAME}:${INPUT_PASSWORD}@gitee.com/$1.git"
 	SOURCE_REPO="$(curl -s "https://gitee.com/api/v5/repos/$1" | jq -r .description)"
-	if ! expr "$SOURCE_REPO" : "https://" >/dev/null; then
-		printf '::error::%s\n' "source repo not found" && return
-	fi
-	TMPDIR="$(mktemp -d)"
-	cd "$TMPDIR" || true
+	expr "$SOURCE_REPO" : "https://" >/dev/null || {
+		RETURN_CODE=1
+		printf '::error::%s\n' "source not found" && return
+	}
+	cd "$(mktemp -d)"
 	git init >/dev/null
 	git remote add source "$SOURCE_REPO" >/dev/null
-	git fetch --all >/dev/null 2>&1 \
-	&& printf 'pull done\n' \
-	|| {
+	git fetch --all >/dev/null 2>&1 && printf 'pull done\n' || {
+		RETURN_CODE=1
 		printf '::error::pull failed\n' && return
 	}
 	git branch -a | grep remotes | grep -v HEAD | awk '{print $1}' | while IFS= read -r BRANCH; do
 		git branch --track "${BRANCH##*/}" "$BRANCH" >/dev/null
 	done
 	git remote add gitee "$GITEE_REPO" >/dev/null
-	git push --all --force gitee >/dev/null 2>&1 \
-	&& git push --tags --force gitee >/dev/null 2>&1 \
-	&& printf 'push done\n' \
-	|| {
+	git push --all --force gitee >/dev/null 2>&1 && git push --tags --force gitee >/dev/null 2>&1 && printf 'push done\n' || {
+		RETURN_CODE=1
 		printf '::error::push failed\n' && return
 	}
 }
 
 # main
+RETURN_CODE=0
 git config --global init.defaultBranch main >/dev/null
 printf '%s' "$INPUT_REPOSITORIES" | while IFS= read -r LINE; do
 	parse_repo "$LINE" | while IFS= read -r REPO; do
@@ -52,4 +52,4 @@ printf '%s' "$INPUT_REPOSITORIES" | while IFS= read -r LINE; do
 	done
 done
 
-exit 0
+exit $RETURN_CODE
