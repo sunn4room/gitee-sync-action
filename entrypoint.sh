d2071c05eee7df7413422b42d9a46ad5ad88d347
sunn4room <sunn4room@163.com>
2024-03-26 19:28:28 +0800

    test log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 parse_repo() {
-	if expr "$1" : '^[^/]*/[^/]*$' >/dev/null; then
+	if expr "$1" : '[^/]*/[^/]*$' >/dev/null; then
 		echo "$1"
 	else
 		echo "$1/111"
@@ -11,7 +11,7 @@ parse_repo() {
 
 printf "$INPUT_REPOSITORIES" | while IFS= read -r LINE; do
 	parse_repo "$LINE" | while IFS= read -r REPO; do
-		echo "::info::$REPO"
+		echo "::notice::$REPO"
 	done
 done
 
