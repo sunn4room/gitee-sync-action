6b66d2a9bc26727c58199986dd7b99c71b030cde
sunn4room <sunn4room@163.com>
2024-03-26 19:21:52 +0800

    handle org

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/sh -l
 
-echo -n "$INPUT_REPOSITORIES" | while IFS= read -r REPO; do
-	echo "$REPO"
+parse_repo() {
+	if expr "$1" : '^[^/]*/[^/]*$' >/dev/null; then
+		echo "$1"
+	else
+		echo "$1/111"
+		echo "$1/222"
+	fi
+}
+
+printf "$INPUT_REPOSITORIES" | while IFS= read -r LINE; do
+	parse_repo "$LINE" | while IFS= read -r REPO; do
+		echo "::info::$REPO"
+	done
 done
 
 exit 0
