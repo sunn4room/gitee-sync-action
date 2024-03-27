8577bd7df6d850d10325d9a36f4a3da3e6658325
sunn4room <sunn4room@163.com>
2024-03-27 08:33:24 +0800

    fix default branch error

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,9 +27,7 @@ mirror_repo() {
 		printf '::error::%s\n' "source not found" && return
 	}
 	cd "$(mktemp -d)"
-	git init >/dev/null
-	git remote add source "$SOURCE_REPO" >/dev/null
-	git fetch --all >/dev/null 2>&1 && printf 'pull done\n' || {
+	git clone "$SOURCE_REPO" . >/dev/null 2>&1 && git fetch --all >/dev/null 2>&1 && printf 'pull done\n' || {
 		RETURN_CODE=1
 		printf '::error::pull failed\n' && return
 	}
